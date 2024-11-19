130a798f6e97ce1b471469ae671d0292d7eec3a8
zhandreii <zhandreidelosangeles44@gmail.com>
2024-11-19 08:16:47 +0800

    Add files via upload

---
+++ 02basicoutput.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Our Lady of Lourdes College Foundation\n";
+	cout << "College of Computer Studies\n";
+	cout << "BSIT-1B\n";
+	cout << "ken zahndrei delos angeles\n";
+	cout << "P-4 brgy talobatib LCN\n";
+	return 0;
+}
